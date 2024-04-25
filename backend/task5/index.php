1e52ac9d8f6ddaf26876ebef7baddef55ef16a57
ChursinAlexUnder <keks63620@gmail.com>
2024-04-25 23:10:41 +0300

    Update index.php

--- backend/task5/index.php
+++ backend/task5/index.php
@@ -316,14 +316,15 @@ else {
 
   }
   else {
-    // Генерируем уникальный логин и пароль.
-    $login = uniqid();
-    $pass = uniqid();
-    // Сохраняем в Cookies.
-    setcookie('login', $login, time() + 12 * 30 * 24 * 60 * 60);
-    setcookie('pass', $pass, time() + 12 * 30 * 24 * 60 * 60);
-
     try {
+      
+      // Генерируем уникальный логин и пароль.
+      $login = uniqid('login_');
+      $pass = uniqid('pass_');
+      // Сохраняем в Cookies.
+      setcookie('login', $login, time() + 12 * 30 * 24 * 60 * 60);
+      setcookie('pass', $pass, time() + 12 * 30 * 24 * 60 * 60);
+
       $stmt = $db->prepare("INSERT INTO users SET fio = ?, tel = ?, email = ?, birth = ?, gender = ?, biography = ?, checkBut = ?");
       $stmt->execute([$_POST['fio'], $_POST['tel'], $_POST['email'], $_POST['day'] . ':' . $_POST['month'] . ':' . $_POST['year'], $_POST['gender'], $_POST['biography'], true]);
   
