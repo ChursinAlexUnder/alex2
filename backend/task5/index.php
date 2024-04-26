5861bddbf1cdfbef1337a7b8dbd60de4e4a27d5c
ChursinAlexUnder <keks63620@gmail.com>
2024-04-27 00:36:40 +0300

    Update index.php

--- backend/task5/index.php
+++ backend/task5/index.php
@@ -303,16 +303,13 @@ else {
 
       $stmt = $db->prepare("INSERT INTO users_languages (id, id_user, id_lang) VALUES (:id, :id_user, :id_lang)");
       foreach ($_POST['languages'] as $id_lang) {
-
-        print($first_id);
-
         // Вставляем $id_lang в БД
         $stmt->bindParam(':id', $first_id);
         $stmt->bindParam(':id_user', $id_user);
         $stmt->bindParam(':id_lang', $id_lang);
         $id_user = $id;
         $stmt->execute();
-        $first_id += 1;
+        $first_id++;
     }
     }
     catch(PDOException $e){
